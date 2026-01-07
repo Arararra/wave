37016703eb3e0258f18273079db49df67b607dec
Arararra <55007364+Arararra@users.noreply.github.com>
2026-01-07 21:11:36 +0700

    setting db prefix

--- config/database.php
+++ config/database.php
@@ -7,4 +7,21 @@ return [
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
     ],
 
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => env('DB_PREFIX', ''),
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+
 ];
